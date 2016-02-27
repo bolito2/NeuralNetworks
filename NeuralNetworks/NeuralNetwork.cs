62fcc67e7b5961244b7072bb3925d70751ed52b0
Bolito2 <bolito2hd@gmail.com>
2016-02-27 16:56:03 +0100

    License and .gitignore

---
+++ NeuralNetworks/NeuralNetwork.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworks
+{
+    public struct Node
+    {
+        public float value;
+        public int id;
+        public bool bias;
+
+        public Node(int id)
+        {
+            this.id = id;
+            value = 0;
+
+            bias = false;
+        }
+    }
+
+    public struct Connection
+    {
+        public float weight;
+        public bool active;
+
+        public Connection(float weight)
+        {
+            this.weight = weight;
+            active = true;
+        }
+    }
+    public class NeuralNetwork
+    {
+        //Input id, output id
+        public Connection[,] connections;
+        //Layer, lid
+        public Node[,] nodes;
+
+        public int nextId = 0;
+
+        private int[] maxNodesPerLayer;
+        public int L;
+
+        int totalNodes = 0;
+
+        public NeuralNetwork(int L, int[] maxNodesPerLayer)
+        {
+            this.L = L;
+            this.maxNodesPerLayer = maxNodesPerLayer;
+
+            nodes = new Node[L, maxNodesPerLayer.Max()];
+            
+            for(int l = 0; l < L; l++)
+            {
+                for(int n = 0; n < maxNodesPerLayer[l]; n++)
+                {
+                    totalNodes++;
+                    nodes[l, n].id = nextId;
+                    nextId++;
+
+                    if (l != L - 1 && n == 0)
+                    {
+                        nodes[l, 0].value = 1;
+                        nodes[l, 0].bias = true;
+                    }
+                }
+            }
+            connections = new Connection[totalNodes, totalNodes];
+        }
+        public void Connect(int inputId, int outputId, float weight)
+        {
+            connections[inputId, outputId] = new Connection(weight);
+        }
+
+        public void SetInput(int[] values)
+        {
+            for(int i = 1; i < maxNodesPerLayer[0]; i++)
+            {
+                nodes[0, i].value = values[i - 1];
+            }
+        }
+
+        public void FeedForward()
+        {
+            for(int l = 1; l < L; l++)
+            {
+                for(int n = 0; n < maxNodesPerLayer[l]; n++)
+                {
+                    for(int c = 0; c < totalNodes; c++)
+                    {
+                        if (connections[c, map(l, n)].active && !nodes[l, n].bias)
+                        {
+                            //Console.WriteLine("Conexion entre l:" + l + ", nodo:" + n + " y l:" + layer(c) + ", nodo:" + lid(c));
+                            nodes[l, n].value += connections[c, map(l, n)].weight * nodes[layer(c), lid(c)].value;
+                        }
+                    }
+                    if(!nodes[l, n].bias)
+                        nodes[l, n].value = sigmoid(nodes[l, n].value);
+                    }
+            }
+            //Prototipo
+            if (nodes[L - 1, 0].value > 0.5)
+                Console.WriteLine("Result of FeedForward is 1");
+            else
+                Console.WriteLine("Result of FeedForward is 0");
+        }
+
+        public int map(int layer, int lid)
+        {
+            return nodes[layer, lid].id;
+        }
+
+        public int layer(int id)
+        {
+            for(int l = 0; l < L; l++)
+            {
+                for(int n = 0; n < maxNodesPerLayer[l]; n++)
+                {
+                    if(nodes[l, n].id == id)
+                    {
+                        return l;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public int lid(int id)
+        {
+            for (int l = 0; l < L; l++)
+            {
+                for (int n = 0; n < maxNodesPerLayer[l]; n++)
+                {
+                    if (nodes[l, n].id == id)
+                    {
+                        return n;
+                    }
+                }
+            }
+            return -1;
+        }
+        public float sigmoid(float z)
+        {
+            return (float)(1 / (1 + Math.Exp(-z)));
+        }
+    }
+}
---
+++ NeuralNetworks/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejemplo(XOR)
+            NeuralNetwork XOR = new NeuralNetwork(3, new int[] {3, 2, 1 });
+
+            XOR.Connect(XOR.map(0, 0), XOR.map(1, 1), -45f);
+            XOR.Connect(XOR.map(0, 1), XOR.map(1, 1), 30f);
+            XOR.Connect(XOR.map(0, 2), XOR.map(1, 1), 30f);
+            XOR.Connect(XOR.map(0, 1), XOR.map(2, 0), 1f);
+            XOR.Connect(XOR.map(0, 2), XOR.map(2, 0), 1f);
+            XOR.Connect(XOR.map(1, 1), XOR.map(2, 0), -10f);
+            
+            Console.WriteLine("Input x1 = 0, x2 = 0 :");
+            XOR.SetInput(new int[] { 0, 0 });
+            XOR.FeedForward();
+
+            Console.WriteLine("Input x1 = 1, x2 = 0 :");
+            XOR.SetInput(new int[] { 1, 0 });
+            XOR.FeedForward();
+
+            Console.WriteLine("Input x1 = 0, x2 = 1 :");
+            XOR.SetInput(new int[] { 0, 1 });
+            XOR.FeedForward();
+
+            Console.WriteLine("Input x1 = 1, x2 = 1 :");
+            XOR.SetInput(new int[] { 1, 1 });
+            XOR.FeedForward();
+
+            Console.ReadKey();
+        }
+    }
+}
